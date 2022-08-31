48bc8a1e2f23359e4c424bfad01c30f32d7ca1de
Денис Долгов <denis.dolgov@monopoly.su>
2022-08-31 21:55:16 +0700

    course list use real data

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,28 @@ import { EthRates, WalletBar } from "@components/web3"
 import { CourseList } from "@components/course";
 import { OrderCard } from "@components/order";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Home() {
+export default function Home({ courses }) {
   return (
     <>
       <Hero />
       <Breadcrumbs />
       <WalletBar />
       <EthRates />
-      <CourseList />
+      <CourseList courses={courses} />
       <OrderCard />
     </>
   )
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses();
+  return {
+    props: {
+      courses: data,
+    },
+  }
 }
 
 Home.Layout = BaseLayout;
