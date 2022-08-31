f44a8cfbc24c1f875fe8fedded3a472bec2c85cf
Денис Долгов <denis.dolgov@monopoly.su>
2022-08-31 20:58:15 +0700

    course page

--- components/course/index.js
+++ components/course/index.js
@@ -1,4 +1,4 @@
 export { default as CourseList } from './list';
 export { default as CourseHero } from './hero';
-export { default as CourseKeyPoints } from './keypoints';
+export { default as KeyPoints } from './keypoints';
 export { default as Curriculum } from './curriculum';
---
+++ pages/course.js
@@ -0,0 +1,18 @@
+import { Modal } from "@components/common";
+import {
+  CourseHero,
+  Curriculum,
+  KeyPoints
+} from "@components/course";
+
+export default function Course() {
+
+  return (
+    <div className="relative max-w-7xl mx-auto px-4">
+      <CourseHero />
+      <KeyPoints />
+      <Curriculum />
+      <Modal />
+    </div>
+  )
+}
