ff6f2cdb2321ba5c40333b52e6700988f6045f45
Денис Долгов <denis.dolgov@monopoly.su>
2022-08-31 20:51:18 +0700

    curriculum component

--- components/course/index.js
+++ components/course/index.js
@@ -1,3 +1,4 @@
 export { default as CourseList } from './list';
 export { default as CourseHero } from './hero';
 export { default as CourseKeyPoints } from './keypoints';
+export { default as Curriculum } from './curriculum';
