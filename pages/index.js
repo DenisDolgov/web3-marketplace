39a41f9349d3344a98c75806e587a5bc6c7763b8
Денис Долгов <denis.dolgov@monopoly.su>
2022-08-31 20:34:18 +0700

    components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
+import { EthRates, WalletBar } from "@components/web3"
 
 export default function Home() {
   return (
-    <>
-      Home
-    </>
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <Navbar />
+          <div className="fit">
+            <Hero />
+            <Breadcrumbs />
+            <WalletBar />
+            <EthRates />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
   )
 }
