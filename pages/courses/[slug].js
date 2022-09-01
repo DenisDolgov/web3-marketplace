6478e2dd3eea97e578ce7b117f4899ff72cc1c7e
Денис Долгов <denis.dolgov@monopoly.su>
2022-09-01 12:00:16 +0700

    course page: getStaticProps

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -5,8 +5,9 @@ import {
   KeyPoints
 } from "@components/course";
 import { BaseLayout } from "@components/layout";
+import {getAllCourses} from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <>
       <div className="py-4">
@@ -17,6 +18,29 @@ export default function Course() {
       <Modal />
     </>
   )
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+
+  return {
+    paths: data.map(c => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export function getStaticProps({ params: { slug } }) {
+  const { data } = getAllCourses();
+
+  const course = data.find(c => c.slug === slug);
+
+  return {
+    props: { course }
+  };
 }
 
 Course.Layout = BaseLayout;
