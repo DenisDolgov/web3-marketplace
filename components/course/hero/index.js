71116d235a370389b1d8c523a14992c3c5a54f45
Денис Долгов <denis.dolgov@monopoly.su>
2022-09-02 12:39:00 +0700

    CourseHero: use next image

--- components/course/hero/index.js
+++ components/course/hero/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Hero = ({ title, description, image }) => {
   return (
     <section>
@@ -39,7 +41,12 @@ const Hero = ({ title, description, image }) => {
           </div>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={image} alt={title} />
+          <Image
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src={image}
+            alt={title}
+            layout="fill"
+          />
         </div>
       </div>
     </section>
