bfefb2c3e02d0d66f9439faea613a3ae0eca001f
Денис Долгов <denis.dolgov@monopoly.su>
2022-09-15 14:55:50 +0700

    use requireInstall

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -5,9 +5,8 @@ import {useAccount} from "@components/hooks/web3";
 import {useRouter} from "next/router";
 
 const Navbar = () => {
-  const { connect, isLoading, web3 } = useWeb3();
-  const { account: accountSwr } = useAccount();
-  const { data: account, isAdmin } = accountSwr;
+  const { connect, isLoading, requireInstall } = useWeb3();
+  const { account } = useAccount();
   const { pathname } = useRouter();
 
   return (
@@ -31,22 +30,22 @@ const Navbar = () => {
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Wishlist</a>
               </Link>
               {
-                isLoading
-                  ? <Button disabled>Loading...</Button>
-                  : web3 !== null
-                    ? account
-                      ? <Button className="cursor-default" hoverable={false}>Hi there{isAdmin && ' Admin'}!</Button>
-                      : <Button onClick={connect}>Connect</Button>
-                    : <Button onClick={() => window.open('https://metamask.io/download/', '_blank')}>Install Metamask</Button>
+                isLoading ?
+                <Button disabled>Loading...</Button> :
+                account.data ?
+                <Button className="cursor-default" hoverable={false}>Hi there{account.isAdmin && ' Admin'}!</Button> :
+                requireInstall ?
+                <Button onClick={() => window.open('https://metamask.io/download/', '_blank')}>Install Metamask</Button> :
+                <Button onClick={connect}>Connect</Button>
               }
             </div>
           </div>
         </nav>
       </div>
-      {account && !pathname.includes('/marketplace') && (
+      {account.data && !pathname.includes('/marketplace') && (
         <div className="flex justify-end sm:px-6 lg:px-8 pt-1">
           <div className="text-white bg-indigo-600 rounded-md p-2">
-            {account}
+            {account.data}
           </div>
         </div>
       )}
