b970aefa2a31a52044a4217d668959aa3c0d5a4e
Денис Долгов <denis.dolgov@monopoly.su>
2022-09-15 10:40:08 +0700

    setupHooks optimize

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -11,6 +11,7 @@ export default function Web3Provider({children}) {
     web3: null,
     contract: null,
     isLoading: true,
+    hooks: setupHooks(),
   });
 
   useEffect(() => {
@@ -24,6 +25,7 @@ export default function Web3Provider({children}) {
           web3,
           contract: null,
           isLoading: false,
+          hooks: setupHooks(web3, provider),
         });
       } else {
         setWeb3Api(api => ({ ...api, isLoading: false }));
@@ -39,8 +41,6 @@ export default function Web3Provider({children}) {
     return {
       ...web3Api,
       isWeb3Loaded: Boolean(web3),
-      getHooks: () => setupHooks(web3, provider),
-      hooks: setupHooks(web3),
       connect: provider
         ? async () => {
           try {
@@ -65,7 +65,7 @@ export function useWeb3() {
 }
 
 export function useHooks(cb) {
-  const { getHooks } = useWeb3();
+  const { hooks } = useWeb3();
 
-  return cb(getHooks());
+  return cb(hooks);
 }
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,25 @@ import { CourseCard, CourseList } from "@ui/course";
 import { OrderCard } from "@ui/order";
 import { BaseLayout } from "@ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
+import {useAccount, useNetwork} from "@components/hooks/web3";
 
 export default function Home({ courses }) {
+  const { network } = useNetwork();
+  const { account } = useAccount();
+
   return (
     <>
       <Hero />
       <Breadcrumbs />
-      <WalletBar />
+      <WalletBar
+        address={account.data}
+        network={{
+          data: network.data,
+          target: network.target,
+          isSupported: network.isSupported,
+          isLoading: network.isLoading,
+        }}
+      />
       <EthRates />
       <CourseList courses={courses}>
         {course => <CourseCard key={course.id} course={course} />}
